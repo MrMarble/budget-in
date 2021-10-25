3cfc43d3bc7e0c59ddc89aa6eec67174b59bc5b4
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-10-26 00:42:11 +0200

    feat(home): basic home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
 
