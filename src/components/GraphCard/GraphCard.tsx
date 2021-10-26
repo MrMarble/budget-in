5a72102a6d771ee74f13eede20c07ceb523c0ea6
Alvaro Tinoco <alv_tinoco_97@hotmail.com>
2021-10-27 00:20:47 +0200

    feat(chart): add bar chart

--- src/components/GraphCard/GraphCard.tsx
+++ src/components/GraphCard/GraphCard.tsx
@@ -1,12 +1,43 @@
-import { Paper } from "@mui/material";
-import { Line } from "react-chartjs-2";
-import { useLineChart } from "./useGraphCard";
+import { ShowChart, BarChart } from "@mui/icons-material";
+import { Paper, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+import LineChart from "../LineChart/LineChart";
+import BarGraph from "../BarChart/BarChart";
 
 export default function GraphCard() {
-  const { data } = useLineChart();
+  const [selectedChart, setSelectedChart] = useState("line");
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newChart: string | null
+  ) => {
+    if (newChart !== null) {
+      setSelectedChart(newChart);
+    }
+  };
+
   return (
     <Paper sx={{ borderRadius: "16px", padding: "1.5rem" }} elevation={3}>
-      <Line data={data} options={{ scales: { y: { min: 0 } } }} />
+      <Box mb={1}>
+        <ToggleButtonGroup
+          exclusive
+          value={selectedChart}
+          onChange={handleChange}
+          size="small"
+        >
+          <ToggleButton value="line">
+            <ShowChart />
+          </ToggleButton>
+          <ToggleButton value="bar">
+            <BarChart />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+      <Box>
+        {selectedChart === "line" && <LineChart />}
+        {selectedChart === "bar" && <BarGraph />}
+      </Box>
     </Paper>
   );
 }
